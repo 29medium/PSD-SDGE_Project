d571941a770de4982e111331d6a0e0e2668baa3a
JoaoSG56 <pg47329@alunos.uminho.pt>
2022-06-04 16:37:27 +0100

    ffff

--- Colector/tcpHandler.erl
+++ Colector/tcpHandler.erl
@@ -1,5 +1,5 @@
 -module(tcpHandler).
--export([run/1, acceptor/2, userHandler/2]).
+-export([run/1]).
 
 run(Port) -> 
 	application:start(chumak),
@@ -18,40 +18,41 @@ run(Port) ->
 	loginManager:run(),
 	LoginManager = whereis(loginManager),
 	io:format("pid: ~p\n",[LoginManager]),
-	acceptor(LSock,LoginManager).
+	acceptor(LSock,LoginManager, PushSocket).
 
-acceptor(LSock,LoginManager) ->
+acceptor(LSock,LoginManager, PushSocket) ->
   {Result, Sock} = gen_tcp:accept(LSock),
   case Result of
 	ok ->
 		io:format("Sucesso: ~p\n",[Result]),
-  		spawn(fun() -> acceptor(LSock,LoginManager) end),
-  		userHandler(Sock,LoginManager);
+  		spawn(fun() -> acceptor(LSock,LoginManager, PushSocket) end),
+  		userHandler(Sock,LoginManager, PushSocket);
 	_ -> 
 		io:format("Erro: ~p\n",[Result]),
-		userHandler(Sock,LoginManager)
+		userHandler(Sock,LoginManager, PushSocket)
 	end.
-userHandler(Sock,LoginManager) ->
+userHandler(Sock,LoginManager, PushSocket) ->
 	receive
 		{_, created} ->
 			inet:setopts(Sock, [{active, once}]),
-			gen_tcp:send(Sock,"Conta criada com sucesso\n"),
+			%gen_tcp:send(Sock,"Conta criada com sucesso\n"),
 			io:format("Conta criada com sucesso\n"),
-			userHandler(Sock, LoginManager);
+			userHandler(Sock, LoginManager, PushSocket);
 		{_,user_exists} -> 
 			inet:setopts(Sock, [{active, once}]),
-			gen_tcp:send(Sock,"User já existe com esse username\n"),
+			%gen_tcp:send(Sock,"User já existe com esse username\n"),
 			io:format("User já existe com esse username\n"),
-			userHandler(Sock, LoginManager);
+			userHandler(Sock, LoginManager, PushSocket);
 		{_ ,logged, User, Type} ->
 			inet:setopts(Sock, [{active, once}]),
-			gen_tcp:send(Sock,"Logged in\n"),
+			%gen_tcp:send(Sock,"Logged in\n"),
 			io:format("Logged in\n"),
-			autenticado(Sock,LoginManager,User,Type);
+			ok = chumak:send(PushSocket,"loggin|"++User++"|"++Type),
+			autenticado(Sock,LoginManager,User, PushSocket);
 		{_ ,invalid} ->
 			inet:setopts(Sock, [{active, once}]),
 			io:format("Invalid\n"),
-			userHandler(Sock, LoginManager);
+			userHandler(Sock, LoginManager, PushSocket);
 		{tcp, _, "register " ++ Data} ->
 			Args = string:tokens(Data, [$\s]),
 			io:format("Register Args: ~p\n",[Args]),
@@ -64,16 +65,16 @@ userHandler(Sock,LoginManager) ->
 						true ->
 							LoginManager ! {create_account,User, Pass1, self()};
 						_ ->
-							gen_tcp:send(Sock,"Palavras pass não coincidem\n"),
+							%gen_tcp:send(Sock,"Palavras pass não coincidem\n"),
 							io:format("Palavras pass não coincidem\n")
 					end,
 					inet:setopts(Sock, [{active, once}]),
-					userHandler(Sock,LoginManager);
+					userHandler(Sock,LoginManager, PushSocket);
 				_ -> 
 					inet:setopts(Sock, [{active, once}]),
 					io:format("bad arguments\n"),
 					
-					userHandler(Sock,LoginManager)
+					userHandler(Sock,LoginManager, PushSocket)
 			end;
 		{tcp, _, "login " ++ Data} ->
 			Args = string:tokens(Data, [$\s]),
@@ -84,11 +85,11 @@ userHandler(Sock,LoginManager) ->
 					Type = string:trim(lists:nth(3,Args)),
 					LoginManager ! {login, User, Pass, Type,self()},
 					inet:setopts(Sock, [{active, once}]),
-					userHandler(Sock,LoginManager);
+					userHandler(Sock,LoginManager, PushSocket);
 				_ -> 
 					inet:setopts(Sock, [{active, once}]),
 					io:format("bad arguments\n"),
-					userHandler(Sock,LoginManager)
+					userHandler(Sock,LoginManager, PushSocket)
 			end;
 		{tcp_closed, _} ->
 			io:format("tcp closed\n"),
@@ -96,22 +97,25 @@ userHandler(Sock,LoginManager) ->
 		{tcp_error, _, _} ->
 			io:format("tcp error\n"),
 			LoginManager ! {logout, self()}
-		%after 10000 -> 
-		%	% jbbj
   	end.
 
-autenticado(Sock,LoginManager,User, Type) ->
+autenticado(Sock,LoginManager,User, PushSocket) ->
 	receive
 		{tcp,_,"event " ++ Data} ->
 			inet:setopts(Sock, [{active, once}]),
-			gen_tcp:send(Sock,"Evento registado\n"),
+			%gen_tcp:send(Sock,"Evento registado\n"),
 			io:format("Evento! ~p\n",[Data]),
-			autenticado(Sock,LoginManager,User,Type);
+			ok = chumak:send(PushSocket,"event|" ++ User ++"|"++Data),
+			autenticado(Sock,LoginManager,User, PushSocket);
 		{tcp, _, "logout\n"} ->
 			LoginManager ! {logout,User,self()},
 			inet:setopts(Sock, [{active, once}]),
-			gen_tcp:send(Sock,"Logged out...\n"),
-			autenticado(Sock,LoginManager,User,Type)
+			ok = chumak:send(PushSocket,"logout|"++User),
+			%gen_tcp:send(Sock,"Logged out...\n"),
+			autenticado(Sock,LoginManager,User, PushSocket)
+		after 5000 -> 
+			io:format("Inativo ~p!!",[User]),
+			ok = chumak:send(PushSocket,"inactive|"++User)
 	end.
 	
 
